beaf7df6613f041bbfdb7df0608f27623f7a3bc0
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-08-06 04:29:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,37 @@ window.addEventListener('keydown', function(event) {
     });
   }
 });
+
+const photoUrls = [
+  "https://www.coolgenerator.com/Data/Textdesign/202308/7e06d36bb7c742ae18934cef5ce3ed5c.png",
+  "https://www.coolgenerator.com/Data/Textdesign/202308/870f6e8946b5a8c4b1e79620af65232e.png",
+  "https://www.coolgenerator.com/Data/Textdesign/202308/ac988f296a074feebf6604a676c5d79c.png",
+  "https://www.coolgenerator.com/Data/Textdesign/202308/2638d0b212740641f4b31b42312eb31e.png",
+  "https://www.coolgenerator.com/Data/Textdesign/202308/85f0c5211187ec83d16707ec0e50354f.png"
+  
+];
+
+const photosContainer = document.querySelector('.photos-container');
+let currentPhotoIndex = 0;
+
+function changePhoto() {
+  if (currentPhotoIndex >= photoUrls.length) {
+      currentPhotoIndex = 0; // Reset index if the end is reached
+  }
+
+  const photo = new Image();
+  photo.src = photoUrls[currentPhotoIndex];
+  photo.style.height = '70px';
+  
+  photosContainer.innerHTML = ''; // Clear existing photo
+  photosContainer.appendChild(photo);
+
+  currentPhotoIndex++;
+
+  // Show the next photo after a delay (0.1 seconds)
+  setTimeout(changePhoto, 100);
+}
+
+// Start changing photos
+changePhoto();
+
